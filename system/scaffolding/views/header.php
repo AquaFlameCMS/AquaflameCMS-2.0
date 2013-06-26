3ea6a3a10057ab1b104ea0cdb008c0723022173e
FailZorD <admin@aquaflame.org>
2013-06-26 17:47:37 +0300

    Update by lev76g
    These are some fixes from lev76g on AquaFlame.NET forums. Thank you.

--- system/scaffolding/views/header.php
+++ system/scaffolding/views/header.php
@@ -1,20 +1,25 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js"><!--<![endif]-->
 <html>
-<head>
-
+        <head>
+                <meta charset="utf-8">
 <title><?php echo $title; ?></title>
-
 <style type='text/css'>
 <?php $this->file(BASEPATH.'scaffolding/views/stylesheet.css'); ?>
 </style>
-
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv='expires' content='-1' />
 <meta http-equiv= 'pragma' content='no-cache' />
-
+<meta name="viewport" content="width=device-width">
 </head>
-<body>
-
+        <body>
+                <!--[if lt IE 7]>
+                        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+                <![endif]-->
 <div id="header">
 <div id="header_left">
 <h3>Scaffolding:&nbsp; <?php echo $title; ?></h3>
@@ -24,6 +29,5 @@
 <?php echo anchor(array($base_uri, 'add'),  $scaff_create_record); ?>
 </div>
 </div>
-
 <br clear="all">
 <div id="outer">
